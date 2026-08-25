fa7d00cff9699748ce7d1e33b00e4ffd6a28d400
Nissy Elsa Sunil <nissysunil702@gmail.com>
2026-08-25 19:33:11 +0530

    Add light and dark mode

--- Frontend--accesslearn/script.js
+++ Frontend--accesslearn/script.js
@@ -147,3 +147,32 @@ if (summaryBox) {
 
     }
 }
+// ===== Theme Toggle =====
+
+const themeToggle = document.getElementById("themeToggle");
+
+// Apply saved theme when page loads
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+}
+
+// Toggle theme
+if (themeToggle) {
+    themeToggle.addEventListener("click", function () {
+
+        document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark");
+            themeToggle.textContent = "☀️ Light Mode";
+        } else {
+            localStorage.setItem("theme", "light");
+            themeToggle.textContent = "🌙 Dark Mode";
+        }
+    });
+
+    // Set correct button text
+    if (document.body.classList.contains("dark-mode")) {
+        themeToggle.textContent = "☀️ Light Mode";
+    }
+}
